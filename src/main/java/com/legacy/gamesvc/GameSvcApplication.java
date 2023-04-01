8ef772f7361d61eeaf504667bf9e865f809c3b44
jbkim4040 <jbkim404037@gmail.com>
2023-04-02 00:08:43 +0900

    게임 api 서비스 시작!

---
+++ src/main/java/com/legacy/gamesvc/GameSvcApplication.java
@@ -0,0 +1,13 @@
+package com.legacy.gamesvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameSvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/legacy/gamesvc/GameSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legacy.gamesvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
